a8deb946f7004a4dd3166c2508b58db230472cb4
phatemeh <phz.mirzaee@gmail.com>
2025-09-21 12:10:34 +0000

    user table completed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->char('mobile', 11)->nullable();
+            $table->enum('role',['user','admin']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
