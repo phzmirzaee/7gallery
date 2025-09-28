78a4ddb2a9508f3d6d484861024a47a8dc5735b3
phatemeh <phz.mirzaee@gmail.com>
2025-09-28 18:29:20 +0000

    frontend blade implemented

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  App\Models\Category;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', function () {
-    echo "Hello World";
+Route::get('create/category', function () {
+    dd(Category::find(1)->update(["title"=>"7lern"]));
+
 });
