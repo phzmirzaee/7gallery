0600b84e3b4d060cd04fd3123a5644da36dda46a
phatemeh <phz.mirzaee@gmail.com>
2025-09-30 16:50:11 +0000

    storing categories implemented

---
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Categories\StoreRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    public function store(StoreRequest $request)
+    {
+       $validatedData= $request->validated();
+        $created_category=Category::create([
+            'title'=>$validatedData['title'],
+            'slug'=> $validatedData['slug'],
+        ]);
+        if(!$created_category){
+            return back()->with('error','دسته بندی ایجاد نشد');
+        }
+        return back()->with('success','دسته بندی ایجاد شد');
+    }
+}
