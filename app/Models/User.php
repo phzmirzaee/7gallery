d5ab9c4365cabbc370f1cf43be241950765ad289
phatemeh <phz.mirzaee@gmail.com>
2025-10-09 14:57:20 +0000

    order implemented

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Product::class);
     }
+
+    public function Orders()
+    {
+        return $this->hasMany(Order::class);
+
+    }
 }
