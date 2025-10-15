deadfa2c38d692e33dd5e22f070e12d1693fd036
phatemeh <phz.mirzaee@gmail.com>
2025-10-15 16:19:48 +0000

    basket feature updated

--- app/Http/Controllers/Home/BasketController.php
+++ app/Http/Controllers/Home/BasketController.php
@@ -12,51 +12,45 @@ class BasketController extends Controller
 {
     public $minutes = 600;
 
-    private function getBasket(?int $productId = null): array
+    private function getBasket(): array
     {
         $basket = json_decode(Cookie::get('basket'), true);
         if (!$basket) {
             $basket = [];
         }
-        $product = null;
-        if ($productId !== null) {
-            $product = Product::findOrFail($productId);
-        }
-
-        return ['product' => $product, 'basket' => $basket];
-
+        return ['basket' => $basket];
     }
 
     public function addToBasket(int $productId): RedirectResponse
     {
-        $data = $this->getBasket($productId);
-        $product = $data['product'];
+        $data = $this->getBasket();
         $basket = $data['basket'];
+
+        $product = Product::findOrFail($productId);
 
         if (isset($basket[$product->id])) {
             return back()->with('success', 'محصول به سبد خرید اضافه شد');
         }
-
         $basket[$product->id] = [
             'title' => $product->title,
             'price' => $product->price,
             'demo_url' => $product->demo_url,
         ];
-
         Cookie::queue('basket', json_encode($basket), $this->minutes);
-
         return back()->with('success', 'محصول به سبد خرید اضافه شد');
     }
 
 
     public function removeFromBasket(int $productId): RedirectResponse
     {
-        $data = $this->getBasket(); // doesn't need productId
+        $data = $this->getBasket();
         $basket = $data['basket'];
-        if (isset($basket[$productId])) {
-            unset($basket[$productId]);
+        if (!isset($basket[$productId])) {
+            return back()->with('error', 'محصول در سبد خرید یافت نشد.');
         }
+
+        unset($basket[$productId]);
         Cookie::queue('basket', json_encode($basket), $this->minutes);
-        return back()->with('success', "محصول از سبد خرید حذف شد");
+        return back()->with('success', "محصول از سبدخرید حذف شد");
     }
 }
