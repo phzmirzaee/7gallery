0f0b00ca4fea7c1aadb0870fd6d0985cfe720ea1
phatemeh <phz.mirzaee@gmail.com>
2025-10-16 08:07:42 +0000

    checkout controller updated

--- app/Http/Controllers/Home/CheckoutController.php
+++ app/Http/Controllers/Home/CheckoutController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\View\View;
 
 class CheckoutController extends Controller
 {
-    public function show()
+    public function show():View
     {
         $products=json_decode(Cookie::get('basket'),true) ?? [];
         $productsPrice=array_sum(array_column($products, 'price'));
